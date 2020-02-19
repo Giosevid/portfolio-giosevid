0e5ee310bd74d4405f235c1037903d5f3c86d4eb
Giosevid <giosevid@gmail.com>
2020-02-19 10:54:57 -0300

    packages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f617b2d5b290309ccaf2";
+/******/ 	var hotCurrentHash = "642e9a2e6bceb3785084";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
